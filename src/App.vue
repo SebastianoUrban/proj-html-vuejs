ce2c7cec57218b4962bd81c88e174671a6e2a7d7
SebastianoUrban <sebastiano.urban98@gmail.com>
2023-01-18 17:39:51 +0100

    scaffolding test

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div>
+    <AppHeader/>
+    <AppMain/>
+    <AppFooter/>
+  </div>
 </template>
+
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
+
+  export default {
+    components : {
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
+  }
+</script>
 
 <style lang="scss">
   @use './style/_variables.sass' as *;
-
 </style>
